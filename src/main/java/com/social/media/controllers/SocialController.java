0dc8635487eb5cf5de742cd43bde55ad1ac8bbb2
p21z0 <pagunuranprim0@gmail.com>
2024-10-04 07:51:05 +0800

    Initial commit

---
+++ src/main/java/com/social/media/controllers/SocialController.java
@@ -0,0 +1,40 @@
+package com.social.media.controllers;
+
+import com.social.media.models.SocialUser;
+import com.social.media.services.SocialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class SocialController {
+    @Autowired
+    private SocialService socialService;
+
+    @GetMapping("/social/users")
+    public ResponseEntity<List<SocialUser>> getUsers(){
+        return new ResponseEntity<>(socialService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping("/social/users")
+    public ResponseEntity<SocialUser> saveUser(
+            @RequestBody SocialUser socialUser
+    ){
+        return new ResponseEntity<>(socialService.saveUser(socialUser), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/social/users/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long userId){
+//        return new ResponseEntity<>(socialService.deleteUser(userId), HttpStatus.OK);
+        socialService.deleteUser(userId);
+        return new ResponseEntity<>("Deleted successfully", HttpStatus.OK);
+
+    }
+
+
+
+
+}
